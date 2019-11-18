2509c03b5fa96d09ea60707c8f7631a66c83f5fc
Vlad <53820373+catprogrammer-cat@users.noreply.github.com>
2019-11-18 18:06:19 +0200

    apply coursework(1-->2)

--- coursework.cpp
+++ coursework.cpp
@@ -37,6 +37,41 @@ int max_value_matrix(int size){ //coursework_1 --> //Вычислить знач
     delete[] array1d;
     return max;
 }
+
+int counter_positive_element_rows_array2d(int size){ //coursework_2 --> //Вычислить элементы вектора как количества 
+    int **array2d = new int * [size];                                   //положительных элементов строк матрицы.
+    for(int i = 0;i<size;i++){
+        array2d[i] = new int [size];
+    }
+    //create array
+    int *array1d = new int[size]; 
+    // fill matrix
+    srand ( time(0) );
+    for(int i = 0; i<size;i++){
+        for(int j = 0;j<size;j++){
+            array2d[i][j] = (rand() % 20)-10;
+        }
+    }
+    // counter positive element
+    int quantity = 0;
+    for(int i=0; i<size; i++){
+        quantity = 0;
+        for(int j=0;j<size;j++){
+            if(array2d[i][j]>=0){
+                quantity++;
+            }
+        }
+        array1d[i] = quantity;
+    }
+    //delete matrix
+    for(int i = 0;i<size;i++){
+        delete[] array2d[i];
+    }
+    delete[] array2d;
+    //delete array1d
+    delete[] array1d;
+    return array1d;
+}
 using namespace std;
 
 int main()
