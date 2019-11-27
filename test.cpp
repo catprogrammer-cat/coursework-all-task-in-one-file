e8150f3d108262c7d8967267dedd0433de3bd05a
Vlad <53820373+catprogrammer-cat@users.noreply.github.com>
2019-11-27 19:44:35 +0200

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -9,8 +9,9 @@
 using namespace std;
 
 // const int M = 3, N = 5;
-void read_and_push_array2d(char* name){
 
+
+void calculate_size_array2d(char* name, int &size_x, int &size_y){
     FILE* f;
     char s[100], *t; // t --> tabulation
     f = fopen(name, "rt"); //открыть файл для чтения как текстовый
@@ -18,7 +19,33 @@ void read_and_push_array2d(char* name){
         cout << "Cannot open file to veiw\n";
     }
 
+    int i, j;
+    // counter size of array2d from FILES
+    for(i=0; fgets(s, 100, f); i++){ // Считывать из файла строки, пока они не закончатся
+        j=0;
+        t = strtok(s, " \t");
+        for(;t != NULL; j++){
+            // array2d[i][j] = atoi(t);
+            t = strtok(NULL, " \t");
+        }
+    }
+    int x, y;
+    size_x = j;
+    size_y = i;
+
+    fclose(f);
+}
+
+
+int** read_and_push_array2d_return(char* name){
     
+    FILE* f;
+    char s[100], *t; // t --> tabulation
+    f = fopen(name, "rt"); //открыть файл для чтения как текстовый
+    if (f == NULL){
+        return 0;
+    }
+
     int i, j;
     // counter size of array2d from FILES
     for(i=0; fgets(s, 100, f); i++){ // Считывать из файла строки, пока они не закончатся
@@ -33,12 +60,16 @@ void read_and_push_array2d(char* name){
     x = j;
     y = i;
 
-
     // create array using size
     //create matrix
     int **array2d = new int * [y];
     for(int i = 0;i<x;i++){
         array2d[i] = new int [x];
+    }
+    fclose(f);
+    f = fopen(name, "rt"); //открыть файл для чтения как текстовый
+    if (f == NULL){
+       return 0;
     }
     // push values into array2d form FILES
     for(i=0; fgets(s, 100, f); i++){ // Считывать из файла строки, пока они не закончатся
@@ -50,43 +81,26 @@ void read_and_push_array2d(char* name){
             t = strtok(NULL, " \t");
         }
     }
-    cout << "x : " << x << endl; // i --> y
-    cout << "y : " << y << endl; // j --> x
-
-    cout << endl;
-
-    // output matrix 
-    for(int i = 0; i<y;i++){
-        cout << i << ":\t";
-        for(int j = 0;j<x;j++){
-            if(array2d[i][j]>=0){
-                cout << "[ " << array2d[i][j]  << "]";
-            }else{
-                cout << "[" << array2d[i][j]  << "]";
-            }
-            
-        }
-        cout << ";" << endl;
-    }
-    // while (fgets(s, 100, f)) { //считывать из файла строки, пока они не закончатся
-    //     j = 0;
-    //     t = strtok(s, " \t");
-    //     while (t != NULL){
-    //         array2d[i][j] = atoi(t);
-    //         t = strtok(NULL, " \t");
-    //         j++;
-    //     }
-    //     i++;
-    // }
     fclose(f);
+    return array2d;
+
 }
 
 int main()
 {
     //double A[M][N];
     int i, j;
+    int size_x;
+    int size_y;
+    
     char  name_1[] = "F1.txt";
     char  name_3[] = "F3.txt";
+
+    // find size of array2d
+    calculate_size_array2d(name_1, size_x,size_y);
+    cout << "size_x = " << size_x << endl;
+    cout << "size_y = " << size_y << endl;
+
     FILE* f3;
     f3 = fopen(name_3, "wt"); //открыть файл для создания как текстовый
     if (f3 == NULL) {
@@ -94,10 +108,15 @@ int main()
         return 1;
     }
     
-    
-    read_and_push_array2d(name_1);
+    int **array2d;
+    array2d = read_and_push_array2d_return(name_1);
     // read_and_push_array2d(name1, double array2d[M][N])
-
+    for(int i=0;i<size_y;i++){
+        for(int j=0;j<size_x;j++){
+            cout << "[" << array2d[i][j] << "]";
+        }
+        cout << endl;
+    }
     
     // create_matrix(name_1, A);
     // for (i = 0; i < M; i++){
