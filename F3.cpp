b865079cb84e78cdffa632c6f32609a284985834
Vlad <53820373+catprogrammer-cat@users.noreply.github.com>
2019-11-28 21:14:57 +0200

    Create F3.cpp

---
+++ F3.cpp
@@ -0,0 +1,70 @@
+Size_y = 8
+
+0:	[ 2][ 3][ 4][ 5][ 4][ 5][ 3][ 2]
+1:	[ 4][ 3][ 2][ 3][ 4][ 5][ 3][ 2]
+2:	[ 4][ 5][ 3][ 2][ 4][ 5][ 3][ 2]
+3:	[-4][ 5][ 3][ 2][ 4][ 5][ 3][ 2]
+4:	[ 4][ 5][ 3][ 2][ 4][ 5][ 3][ 2]
+5:	[-4][ 5][ 3][ 2][ 4][ 5][ 3][ 2]
+6:	[ 4][ 5][ 3][ 2][ 4][ 5][ 3][ 2]
+7:	[ 4][ 5][ 3][ 2][ 4][ 5][ 3][ 2]
+------------------------------------------
+Coursework 1, task 1: "Calculate the maximum value element of the main diagonal of the matrix."
+Max value of main diagonal of matrix : 5 <----- If width and height of matrix will not be equal , then return 0;(diagonal doesen't exist)
+------------------------------------------
+------------------------------------------
+Coursework 1, task 2: "Calculate vector elements as quantities positive elements of the rows of the matrix."
+Vector of quantities positive elements of the rows of the matrix : [8][8][8][7][8][7][8][8]
+------------------------------------------
+------------------------------------------
+Coursework 1, task 3: "determine: the sum of the elements in those columns that do not contain negative elements."
+Vector of sum of the elements of the columns of the matrix that do not contain negative elements : [0][36][24][20][32][40][24][16]
+------------------------------------------
+------------------------------------------
+Coursework 1, task 4: "Find the minimum among the sums of the modules of the elements of the diagonals parallel to the side diagonal of the matrix."
+Vector of sum of the side diagonal of the matrix: [7][11][16][19][23][26][28][24][19][16][14][10][5]
+Min element : 5
+Index of min element : 12
+------------------------------------------
+------------------------------------------
+Coursework 2, task 1: "Create a one-dimensional array of matrix elements."
+[2][3][4][5][4][5][3][2][4][3][2][3][4][5][3][2][4][5][3][2][4][5][3][2][-4][5][3][2][4][5][3][2][4][5][3][2][4][5][3][2][-4][5][3][2][4][5][3][2][4][5][3][2][4][5][3][2][4][5][3][2][4][5][3][2]
+------------------------------------------
+------------------------------------------
+Coursework 3, task 1: "Determine if the string contains the letter X (small or large)."
+xxxxFAn array is a collection of elements of the same type placed in contiguous memory locations that can be individually referenced by using an index to a unique identifier.
+    ++This string has x
+For example, a five element integer array foo may be logically represented as;
+    --This string hasn't x/X
+the index of the first array element is always zero. As expected, an n array must be declared prior its use. A typical declaration for an array in C++ is:
+    --This string hasn't x/X
+where type is a valid type (such as int, float ...), name is a valid identifier and the elements field (which is always enclosed in square brackets []), specifies the size of the array.
+    --This string hasn't x/X
+int foo [5];
+    --This string hasn't x/X
+: The elements field within square brackets [], representing the number of elementsin the array, must be a constant expression, since arrays are blocks of static memory whose size must be known at co    --This string hasn't x/X
+INITIALIZING ARRAYS
+    --This string hasn't x/X
+The initializer can even have no values, just the braces:
+    --This string hasn't x/X
+This creates an array of five int values, each initialized with a value of zero:
+    --This string hasn't x/X
+int foo [5] = { 16, 2, 77, 40, 12071 };  
+    --This string hasn't x/X
+The number of values between braces {} shall not be greater than the number of elements in the array. For example, in the example above, foo was declared having 5 elements (as specified by the number    --This string hasn't x/X
+ntal types, means they are filled with zeroes). For example:
+    --This string hasn't x/X
+Will create an array like this:
+    --This string hasn't x/X
+ that matches the number of values included between the braces {}:
+    --This string hasn't x/X
+After this declaration, array foo would be five int long, since we have provided five initialization values.
+    --This string hasn't x/X
+oth these statements are equivalent:
+    --This string hasn't x/X
+int foo[] { 10, 20, 30 }; 
+    --This string hasn't x/X
+Static arrays, and those declared directly in a namespace (outside any function), are always initialized. If no explicit initializer is specified, all the elements are default-initialized (with zeroe    --This string hasn't x/X
+s, for fundamental types).    --This string hasn't x/X
+
+------------------------------------------
